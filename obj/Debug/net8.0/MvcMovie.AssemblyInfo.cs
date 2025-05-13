1e3b8f0dc3a007bbd452f029579ff16991cec43d
Islam Hassan <islam.abuaish@gmail.com>
2025-05-13 13:36:40 +0300

    Add image preview functionality and improve product display layout

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c616f9da8e587a75e2bd62e368399e8c5c015166")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eca64443068113e5cd5e49fd5a6cb1e515078716")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

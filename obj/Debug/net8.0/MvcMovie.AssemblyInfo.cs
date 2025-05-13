eca64443068113e5cd5e49fd5a6cb1e515078716
Islam Hassan <islam.abuaish@gmail.com>
2025-05-13 13:28:19 +0300

    Enhance product display with stock status badges and update assembly info

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ed27d9afd869fb576450c4e3e95c8f20069f33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c616f9da8e587a75e2bd62e368399e8c5c015166")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

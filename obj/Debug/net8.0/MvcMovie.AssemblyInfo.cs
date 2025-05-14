4e88f0aab8ab8cef7d2b8bac62ec2f43b2534257
Islam Hassan <islam.abuaish@gmail.com>
2025-05-14 13:34:47 +0300

    Update AssemblyInformationalVersion and cache inputs for consistency

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3b8f0dc3a007bbd452f029579ff16991cec43d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2317b961c6c0df6527bc5170795c8ddb6932373")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
